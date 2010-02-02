f9aa82a90dd2af1f8525715e05bca69305356e1e
Pavel Krusek <krtz@Pavel-Kruseks-MacBook-Pro.local>
2010-02-02 11:41:20 +0100

    new button banner

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,7 +3,7 @@ class Admin::AdminController < ResourceController::Base
   layout 'admin'
   
   before_filter :set_values
-  
+  before_filter :check_authorization
   
   protected
       
@@ -13,6 +13,18 @@ class Admin::AdminController < ResourceController::Base
     
     def set_values
       @kraje = [["CZ011","Hlavní město Praha"],["CZ021","Středočeský kraj"],["CZ031","Jihočeský kraj"],["CZ032","Plzeňský kraj"],["CZ041","Karlovarský kraj"],["CZ042","Ústecký kraj"],["CZ051","Liberecký kraj"],["CZ052","Královéhradecký kraj"],["CZ053","Pardubický kraj"],["CZ061","Vysočina"],["CZ062","Jihomoravský kraj"],["CZ071","Olomoucký kraj"],["CZ072","Zlínský kraj"],["CZ081","Moravskoslezský kraj"]]
+    end
+    
+  private
+  
+    def check_authorization
+      unless current_user && current_user.id = 1
+        #store_location
+        flash[:error] = "Musíte se přihlásit pro přístup na tuto stránku."
+        redirect_to new_user_session_url
+        return false
+      end  
     end  
+      
   
 end
